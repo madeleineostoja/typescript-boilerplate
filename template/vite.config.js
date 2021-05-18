e2dec2bd273a344c98465c69efae1443bf676565
madeleineostoja <madi@madeleineostoja.com>
2021-05-18 13:21:03 +1200

    fix templating

--- template/vite.config.js
+++ template/vite.config.js
@@ -4,7 +4,7 @@ module.exports = {
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: '<% name %>'
+      name: '<%= name %>'
     },
     rollupOptions: {
       external: []
